ef51bf03954406de36ab0e401f2edcb001ad7f98
Alan G. Heredia <hgarcia.alan@gmail.com>
2021-05-10 09:41:57 -0700

    Register new doctors

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 
 from appointments.views.index import index
-from appointments.views.doctors import DoctorListView
 from appointments.views import appointments
+from appointments.views import doctors
 from appointments.views.patients import PatientListView
 
 urlpatterns = [
     path('', index, name='index'),
     path('citas/', appointments.AppointmentListView.as_view(), name='appointments'),
     path('citas/agendar_cita/', appointments.AppointmentCreateView.as_view(), name='agendar_cita'),
-    path('doctores/', DoctorListView.as_view(), name='doctors'),
+
+    path('doctores/', doctors.DoctorListView.as_view(), name='doctors'),
+    path('doctores/registrar_doctor/', doctors.DoctorCreateView.as_view(), name='registrar_doctor'),
+    
     path('pacientes/', PatientListView.as_view(), name='patients'),
 ]
--- appointments/views/doctors.py
+++ appointments/views/doctors.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponseRedirect
-from django.views import generic
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 from appointments.models.doctor import Doctor
 
-class DoctorListView(generic.ListView):
+class DoctorListView(ListView):
     model = Doctor
     template_name = 'doctors/doctors.html'
     paginate_by = 25
@@ -11,3 +12,9 @@ class DoctorListView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+class DoctorCreateView(CreateView):
+    model = Doctor
+    fields = ('name', 'first_last_name', 'second_last_name', 'birthday', 'speciality')
+    template_name = 'doctors/doctor_form.html'
+    success_url = reverse_lazy('doctors')
